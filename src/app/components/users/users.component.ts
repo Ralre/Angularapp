44124818510c940d256c6c7053d0f90336aa5195
Ralre <matthew.kerr.7.ctr@us.af.mil>
2019-03-19 11:10:34 -0500

    Lesson 29 Complete

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,12 +11,7 @@ export class UsersComponent implements OnInit {
   user: User = {
     firstName: '',
     lastName: '',
-    age: null,
-    address: {
-      street: '',
-      city: '',
-      state: ''
-    }
+    email: ''
   };
   users: User[];
   showExtended: boolean = true;
@@ -33,12 +28,7 @@ export class UsersComponent implements OnInit {
         {
           firstName: 'John',
           lastName: 'Doe',
-          age: 70,
-          address: {
-            street: '50 Main St',
-            city: 'Boston',
-            state: 'MA'
-            },
+          email: 'john@gmail.com',
             isActive: true,
             registered: new Date('01/02/2018 08:30:00'),
             hide: true
@@ -46,12 +36,7 @@ export class UsersComponent implements OnInit {
           {
             firstName: 'Kevin',
             lastName: 'Johnson',
-            age: 34,
-            address: {
-              street: '20 School St',
-              city: 'Lynn',
-              state: 'MA'
-            },
+            email: 'kevin@yahoo.com',
             isActive: false,
             registered: new Date('03/11/2017 06:20:00'),
             hide: true
@@ -59,12 +44,7 @@ export class UsersComponent implements OnInit {
           {
             firstName: 'Karen',
             lastName: 'Williams',
-            age: 26,
-            address: {
-              street: '55 Mill St',
-              city: 'Miami',
-              state: 'FL'
-            },
+            email: 'karen@gmail.com',
             isActive: true,
             registered: new Date('11/02/2016 10:30:00'),
             hide: true
@@ -75,37 +55,23 @@ export class UsersComponent implements OnInit {
 
   }
 
-  addUser() {
-    this.user.isActive = true;
-    this.user.registered = new Date();
-    this.users.unshift(this.user);
+  // addUser() {
+  //   this.user.isActive = true;
+  //   this.user.registered = new Date();
+  //   this.users.unshift(this.user);
 
-    this.user = {
-      firstName: '',
-      lastName: '',
-      age: null,
-      address: {
-        street: '',
-        city: '',
-        state: ''
-      }
-    };
-  }
-
-
-  // toggleHide(user: User) {
-  //   user.hide = !user.hide ;
-
+  //   this.user = {
+  //     firstName: '',
+  //     lastName: '',
+  //     email: ''
+  //   };
   // }
+
   onSubmit(e){
     console.log(123);
     e.preventDefault();
   }
 
-  fireEvent(e ) {
-    console.log(e.type);
-    console.log(e.target.value);
 
-  }
 
 }
